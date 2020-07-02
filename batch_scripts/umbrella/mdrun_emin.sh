f41f8583cb39c485082400c08dc4ff30e658819c
Sebastian <sebastianmoes@gmail.com>
2020-07-02 16:41:50 +0200

    get_distances and clean up

--- batch_scripts/umbrella/mdrun_emin.sh
+++ batch_scripts/umbrella/mdrun_emin.sh
@@ -13,6 +13,7 @@
 # specify number of threads per task
 #SBATCH -c 1
 
+
 # It is always best to do a ml purge before loading modules in a submit file
 ml purge
 ml ABINIT/8.10.3 Armadillo/9.700.2 CDO/1.9.5 GOTM/5.3-221-gac7ec88d NCO/4.8.1 NCO/4.9.2 OpenFOAM/6 OpenFOAM/7 OpenFOAM/v1912 Rosetta/3.7 Siesta/4.1-MaX-1.0 Siesta/4.1-b4 Singular/4.1.2 XCrySDen/1.5.60 XCrySDen/1.6.2 deal.II/9.1.1-gcc deal.II/9.1.1-intel

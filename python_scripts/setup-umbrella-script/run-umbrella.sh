976179eff67d7386df2dbf1ca1d818af977f253d
Tjmcawesome <32533387+Tjmcawesome@users.noreply.github.com>
2020-07-27 16:00:57 +0200

    Various fixes to batch stripts, and adding TB HSP sequence files

--- python_scripts/setup-umbrella-script/run-umbrella.sh
+++ python_scripts/setup-umbrella-script/run-umbrella.sh
@@ -9,7 +9,7 @@
 #request a certain number of hours for the run HR:MIN:SEC
 #SBATCH -t 02:00:00
 # specify number of cores you want
-#SBATCH -n 1
+#SBATCH -n 20
 # specify number of threads per task
 #SBATCH -c 1
 
@@ -20,9 +20,9 @@ ml ABINIT/8.10.3 Armadillo/9.700.2 CDO/1.9.5 GOTM/5.3-221-gac7ec88d NCO/4.8.1 NC
 ml gromacs/2019.6.th
 
 # Short equilibration
-gmx grompp -f npt_umbrella.mdp -c confXXX.gro -r confXXX.gro -p topol.top -n index.ndx -o nptXXX.tpr
+gmx grompp -f npt_umbrella.mdp -c confXXX.gro -r confXXX.gro -p topol.top -n index.ndx -o nptXXX.tpr -maxwarn 2
 gmx mdrun -deffnm nptXXX
 
 # Umbrella run
-gmx grompp -f md_umbrella.mdp -c nptXXX.gro -r nptXXX.gro -t nptXXX.cpt -p topol.top -n index.ndx -o umbrellaXXX.tpr
+gmx grompp -f md_umbrella.mdp -c nptXXX.gro -r nptXXX.gro -t nptXXX.cpt -p topol.top -n index.ndx -o umbrellaXXX.tpr -maxwarn 2
 gmx mdrun -deffnm umbrellaXXX

a82868d9a696a706724173d0017a9d466d0d13bf
seb moes <sebastianmoes@gmail.com>
2020-08-27 00:46:21 +0200

    Slight fix to correlatiion test

--- python_scripts/correlation_test.py
+++ python_scripts/correlation_test.py
@@ -4,22 +4,18 @@
 # %%
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import spearmanr, pearsonr
 
-
 # %%
-#import data
+# import data
 sele_data = 'docking_sele.csv'
 sele_df = pd.read_csv(sele_data, sep=',')
-
 
 # %%
 
 
 for pose, i in enumerate(range(0, 44, 4)):
-    x = sele_df['Avg Affinity Kd'][i:i+4]
+    x = sele_df['Avg Affinity Kd'][i:i + 4]
     exp = sele_df['Avg Affinity Kd'][40:44]
     correlation, pval = spearmanr(x, exp)
     print('pose ', str(pose), 'compared to experimental - spearman')
